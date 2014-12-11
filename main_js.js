203ca77d058b7e66e05c974285eedd56dffa3499
CamiWilliams <lambacam@iastate.edu>
2014-12-11 11:10:32 -0600

    Bug fixes
    Cancel button doesn’t work

--- main_js.js
+++ main_js.js
@@ -29,7 +29,6 @@ function loadContent() {
 	
 	myDataRef = new Firebase(path + "submitted/");
 	myDataRef.once('value', function(snapshot) {
-		var x = snapshot.numChildren();	
 		snapshot.forEach(function(childSnapshot) {
 			if(childSnapshot.val() != null) {		
 				toFill.innerHTML += fill(childSnapshot.val().name, childSnapshot.val().description, childSnapshot.val().imageSource);	
@@ -83,15 +82,15 @@ function handleFileSelect(evt) {
 	
 	var f = evt.files[0];
 	var reader = new FileReader();
+
 	reader.onload = (function(theFile) {
 		return function(e) {
 			var filePayload = e.target.result;
+	        var hash = CryptoJS.SHA256(Math.random() + CryptoJS.SHA256(filePayload));
 			myDataRef = new Firebase(path + "to_review/");
 			myDataRef.once('value', function(snapshot) {
-				var x = snapshot.numChildren() + 1;	
-	
 				var dataRef = new Firebase(path + "to_review/");
-				dataRef.child(x).set({name: name, email: email, description: descr, imageSource: e.target.result});
+				dataRef.child(hash.words[0]).set({name: name, email: email, description: descr, imageSource: e.target.result});
 			});
 		};
 	})(f);
@@ -99,15 +98,75 @@ function handleFileSelect(evt) {
 }
 
 function toReviewBoard() {
+	var toFill = document.getElementById("manager-container");
+	toFill.innerHTML = " ";
 	
+	myDataRef = new Firebase(path + "to_review/");
+	myDataRef.once('value', function(snapshot) {
+		snapshot.forEach(function(childSnapshot) {
+			if(childSnapshot.val() != null) {		
+				toFill.innerHTML += fillReviewManager(childSnapshot.val().name, childSnapshot.val().description, childSnapshot.val().imageSource, childSnapshot.name());	
+			}
+		});
+	});
 }
+
+function fillReviewManager(name, description, image, uniqueIdentifier) {
+	return "<div class='row'> <div class='col-sm-6 col-md-4'> <div class='thumbnail'>"
+ 			+ "<img src='" + image + "'> <div class='caption'> <h3>" + name + "</h3>"
+			+ "<p id='" + uniqueIdentifier + "'>" + description + "</p></div>"
+			+ "<button onclick='editManager(1," + uniqueIdentifier +")' class='btn btn-default btn-sm'>Edit</button>"
+			+ "<button onclick='acceptReviewManager("+ uniqueIdentifier +")' class='btn btn-default btn-sm'>Accept</button>"
+			+ "<button onclick='removeManager(1," + uniqueIdentifier +")' class='btn btn-default btn-sm'>Deny</button></div></div></div>";
+}
+
+function acceptReviewManager(id) {
+	var agree=confirm("Are you sure you want to confirm the changes you made to this submission?");
+	if (agree) {
+	myDataRef = new Firebase(path + "to_review/" + id);
+	myDataRef.once('value', function(snapshot){
+		var dataRef = new Firebase(path + "submitted/");
+		dataRef.once('value', function(snapshot2) {
+					
+		dataRef.child(myDataRef.name()).set({name: snapshot.val().name, email: snapshot.val().email, description: snapshot.val().description, imageSource: snapshot.val().imageSource});
+		});
+		myDataRef.remove();
+	});
+	alert("Refresh page to see changes.");
+	}
+}
+
+function editManager(num, id) {
+	var sor = "to_review";
+	if(num == 0) { sor = "submitted"; }
+	myDataRef = new Firebase(path + sor + "/" + id);
+	myDataRef.once('value', function(snapshot) {
+		var toFill = document.getElementById(id);
+		toFill.innerHTML += "<div class='input-group-"+ snapshot.name() + "'>" + "<textarea id='edit-descr-input-"+ snapshot.name() +"' type='text' rows='4' cols='50'>" + snapshot.val().description + "</textarea>" 
+			+ "<button onclick='confirmEditManager(" + num + "," + snapshot.name() +")' class='btn btn-default btn-sm'>Confirm</button>"
+			+ "<button onclick='cancelEditManager(" + snapshot.name() + "," + snapshot.val().description + ")' class='btn btn-default btn-sm'>Cancel</button>";
+	});
+}
+
+function confirmEditManager(num, id) {
+	var sor = "to_review";
+	if(num == 0) { sor = "submitted"; }
+	var agree=confirm("Are you sure you want to confirm the changes you made to this submission?");
+	if (agree) {
+		myDataRef = new Firebase(path + sor + "/" + id);
+		myDataRef.child("description").set(document.getElementById("edit-descr-input-"+ id).value);
+		var toFill = document.getElementById(id);
+		toFill.innerHTML = document.getElementById("edit-descr-input-"+ id).value;
+	}
+}
+
 
 function submittedBoard() {
 	var toFill = document.getElementById("manager-container");
+	toFill.innerHTML = " ";
 	
 	myDataRef = new Firebase(path + "submitted/");
 	myDataRef.once('value', function(snapshot) {
-		var x = snapshot.numChildren();	
 		snapshot.forEach(function(childSnapshot) {
 			if(childSnapshot.val() != null) {		
 				toFill.innerHTML += fillSubmitManager(childSnapshot.val().name, childSnapshot.val().description, childSnapshot.val().imageSource, childSnapshot.name());	
@@ -117,43 +176,25 @@ function submittedBoard() {
 }
 
 function fillSubmitManager(name, description, image, uniqueIdentifier) {
+	var sor = "submitted";
 	return "<div class='row'> <div class='col-sm-6 col-md-4'> <div class='thumbnail'>"
  			+ "<img src='" + image + "'> <div class='caption'> <h3>" + name + "</h3>"
 			+ "<p id='" + uniqueIdentifier + "'>" + description + "</p></div>"
-			+ "<button onclick='editSubmitManager("+ uniqueIdentifier +")' class='btn btn-default btn-sm'>Edit</button>"
-			+ "<button onclick='removeSubmitManager("+ uniqueIdentifier +")' class='btn btn-default btn-sm'>Remove</button></div></div></div>";
+			+ "<button onclick='editManager(0," + uniqueIdentifier +")' class='btn btn-default btn-sm'>Edit</button>"
+			+ "<button onclick='removeManager(0," + uniqueIdentifier +")' class='btn btn-default btn-sm'>Remove</button></div></div></div>";
 }
 
-function editSubmitManager(id) {
-	
-	myDataRef = new Firebase(path + "submitted/" + id);
-	myDataRef.once('value', function(snapshot) {
-		var toFill = document.getElementById(id);
-		toFill.innerHTML += "<div class='input-group'>" + "<textarea id='edit-descr-input-"+ snapshot.name() +"' type='text' rows='4' cols='50'>" + snapshot.val().description + "</textarea>" 
-			+ "<button onclick='confirmEditSubmitManager("+ snapshot.name() +")' class='btn btn-default btn-sm'>Confirm</button>"
-			+ "<button onclick='cancelEditSubmitManager("+ snapshot.name() + "," + snapshot.val().description + ")' class='btn btn-default btn-sm'>Cancel</button>";
-	});
+function cancelEditManager(id, des) {
+	var toFill = document.getElementById(id);
+	toFill.innerHTML = des;
 }
 
-function confirmEditSubmitManager(id) {
-	var agree=confirm("Are you sure you want to confirm the changes you made to this submission?");
-	if (agree) {
-		myDataRef = new Firebase(path + "submitted/" + id);
-		myDataRef.child("description").set(document.getElementById("edit-descr-input-"+ id).value);
-		var toFill = document.getElementById(id);
-		toFill.innerHTML = document.getElementById("edit-descr-input-"+ id).value;
-	}
-}
-
-function cancelEditSubmitManager(id, des) {
-		var toFill = document.getElementById(id);
-		toFill.innerHTML = des;
-}
-
-function removeSubmitManager(id) {
+function removeManager(num, id) {
+	var sor = "to_review";
+	if(num == 0) { sor = "submitted"; }
 	var agree=confirm("Are you sure you want to remove this submission?");
 	if (agree) {
-		myDataRef = new Firebase(path + "submitted/" + id);
+		myDataRef = new Firebase(path + sor + "/" + id);
 		myDataRef.remove();
 		alert("Refresh page to see changes.");
 		//submittedBoard();
